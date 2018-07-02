1417ebf0d85649e789b9f662ef6f4b2fabb14c46
Ryan Long <rlong@redhat.com>
2018-07-02 13:22:51 -0400

    lint

--- src/SmartComponents/Rules/Rules.js
+++ src/SmartComponents/Rules/Rules.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import asyncComponent from '../../Utilities/asyncComponent';
 
-import { PageHeader, PageHeaderTitle } from '@red-hat-insights/insights-frontend-components'
+import { PageHeader, PageHeaderTitle } from '@red-hat-insights/insights-frontend-components';
 
 const ListRules = asyncComponent(() => import(/* webpackChunkName: "ListRules" */ './ListRules'));
 const ViewRule = asyncComponent(() => import(/* webpackChunkName: "ListRules" */ './ViewRule'));
