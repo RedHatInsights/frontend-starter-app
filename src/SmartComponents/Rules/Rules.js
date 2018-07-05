128b1433349d101b1767567b8587fd503a576fb9
Ryan Long <rlong@redhat.com>
2018-07-05 13:02:38 -0400

    moving presentational rules components

--- src/SmartComponents/Rules/Rules.js
+++ src/SmartComponents/Rules/Rules.js
@@ -4,8 +4,8 @@ import asyncComponent from '../../Utilities/asyncComponent';
 
 import { PageHeader, PageHeaderTitle } from '@red-hat-insights/insights-frontend-components';
 
-const ListRules = asyncComponent(() => import(/* webpackChunkName: "ListRules" */ './ListRules'));
-const ViewRule = asyncComponent(() => import(/* webpackChunkName: "ListRules" */ './ViewRule'));
+const ListRules = asyncComponent(() => import(/* webpackChunkName: "ListRules" */ '../../PresentationalComponents/Rules/ListRules'));
+const ViewRule = asyncComponent(() => import(/* webpackChunkName: "VoewRule" */ '../../PresentationalComponents/Rules/ViewRule'));
 
 const Rules = () => {
     return (
