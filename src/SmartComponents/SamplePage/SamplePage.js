538b8a948bd04381653611662c723a45591dbba4
Ryan Long <rlong@redhat.com>
2018-09-05 12:05:10 -0400

    updating layout to use new chroming and layout components

--- src/SmartComponents/SamplePage/SamplePage.js
+++ src/SmartComponents/SamplePage/SamplePage.js
@@ -3,9 +3,7 @@ import { withRouter } from 'react-router-dom';
 import asyncComponent from '../../Utilities/asyncComponent';
 import './sample-page.scss';
 
-import { PageHeader } from '@red-hat-insights/insights-frontend-components';
-import { PageHeaderTitle } from '@red-hat-insights/insights-frontend-components';
-import { Section } from '@red-hat-insights/insights-frontend-components';
+import { Section, Main, PageHeader, PageHeaderTitle } from '@red-hat-insights/insights-frontend-components';
 
 import { Button } from '@patternfly/react-core';
 
@@ -25,19 +23,22 @@ class SamplePage extends Component {
             <React.Fragment>
                 <PageHeader>
                     <PageHeaderTitle title='Sample Insights App'/>
+                    <p> This is page header text </p>
                 </PageHeader>
-                <Section type='content'>
-                    <h1> Sample Component </h1>
-                    <SampleComponent> Sample Component </SampleComponent>
-                    <h1> Cards </h1>
-                    <h1> Buttons </h1>
-                    <Section type='button-group'>
-                        <Button variant='primary'> PF-Next Primary Button </Button>
-                        <Button variant='secondary'> PF-Next Secondary Button </Button>
-                        <Button variant='tertiary'> PF-Next Tertiary Button </Button>
-                        <Button variant='danger'> PF-Next Danger Button </Button>
+                <Main>
+                    <Section type='content'>
+                        <h1> Sample Component </h1>
+                        <SampleComponent> Sample Component </SampleComponent>
+                        <h1> Cards </h1>
+                        <h1> Buttons </h1>
+                        <Section type='button-group'>
+                            <Button variant='primary'> PF-Next Primary Button </Button>
+                            <Button variant='secondary'> PF-Next Secondary Button </Button>
+                            <Button variant='tertiary'> PF-Next Tertiary Button </Button>
+                            <Button variant='danger'> PF-Next Danger Button </Button>
+                        </Section>
                     </Section>
-                </Section>
+                </Main>
             </React.Fragment>
         );
     }
