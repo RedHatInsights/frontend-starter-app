7bdf6d70417108258c0bf6c5942c3328ea901fdd
Allen Wight <awight@redhat.com>
2018-10-11 11:51:46 -0400

    Updates babel from 6 👉 7
    All npm scripts perform nominally

--- config/setupTests.js
+++ config/setupTests.js
@@ -1,7 +1,6 @@
 import { configure, mount, render, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import 'babel-polyfill';
 
 configure({ adapter: new Adapter() });
 
