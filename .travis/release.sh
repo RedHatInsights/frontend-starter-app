daafec7f9d818ccf5722a6fb7b32ac3b4d3cf320
Josh <jkinlaw@redhat.com>
2018-10-30 08:24:08 -0400

    adds dev commit message as build commit message

--- .travis/release.sh
+++ .travis/release.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
 git clone ${REPO}.git -b $1
 cd dist
 cp ../${REPO_DIR}/Jenkinsfile ./Jenkinsfile
@@ -9,6 +8,6 @@ git config --global user.name $COMMIT_AUTHOR_USERNAME
 git config --global user.email $COMMIT_AUTHOR_EMAIL
 git remote add travis-build ${REPO}.git
 git add .
-git commit -m 'Build by Travis'
+git commit -m "${TRAVIS_COMMIT_MESSAGE}"
 git push --force --set-upstream travis-build HEAD:$1
 
