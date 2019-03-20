f9c70b44ba9da4b51a258f85976a03227062f680
Karel Hala <ch4rlien@gmail.com>
2019-03-20 18:48:57 +0100

    Add beta in basename if on beta release (#63)

--- src/entry-dev.js
+++ src/entry-dev.js
@@ -9,13 +9,14 @@ import logger from 'redux-logger';
 const pathName = window.location.pathname.split('/');
 pathName.shift();
 
+let release = '/';
 if (pathName[0] === 'beta') {
-    pathName.shift();
+    release = `/beta/${pathName.shift()}`;
 }
 
 ReactDOM.render(
     <Provider store={ init(logger).getStore() }>
-        <Router basename={ `${pathName[0]}/${pathName[1]}` }>
+        <Router basename={ `${release}${pathName[0]}/${pathName[1]}` }>
             <App/>
         </Router>
     </Provider>,
