a8905b5f4f7a0b6764fc941c69c06a49345ae4e4
Ian Page Hands <iphands@gmail.com>
2019-04-08 08:46:40 -0400

    unbake the port for the frontend

--- profiles/local-frontend-and-api.js
+++ profiles/local-frontend-and-api.js
@@ -2,13 +2,15 @@
 
 const SECTION = 'insights';
 const APP_ID = 'starter';
+const FRONTEND_PORT = 8002;
 const API_PORT = 8888;
 const routes = {};
 
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: 'http://localhost:8002' };
-routes[`/${SECTION}/${APP_ID}`] = { host: 'http://localhost:8002' };
-routes[`/beta/apps/${APP_ID}`] = { host: 'http://localhost:8002' };
-routes[`/apps/${APP_ID}`] = { host: 'http://localhost:8002' };
+
+routes[`/beta/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/${SECTION}/${APP_ID}`]      = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/beta/apps/${APP_ID}`]       = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`]            = { host: `http://localhost:${FRONTEND_PORT}` };
 
 routes[`/api/${APP_ID}`] = { host: `http://localhost:${API_PORT}` };
 
--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -2,11 +2,12 @@
 
 const SECTION = 'insights';
 const APP_ID = 'starter';
+const FRONTEND_PORT = 8002;
 const routes = {};
 
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: 'http://localhost:8002' };
-routes[`/${SECTION}/${APP_ID}`] = { host: 'http://localhost:8002' };
-routes[`/beta/apps/${APP_ID}`] = { host: 'http://localhost:8002' };
-routes[`/apps/${APP_ID}`] = { host: 'http://localhost:8002' };
+routes[`/beta/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/${SECTION}/${APP_ID}`]      = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/beta/apps/${APP_ID}`]       = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`]            = { host: `http://localhost:${FRONTEND_PORT}` };
 
 module.exports = { routes };
