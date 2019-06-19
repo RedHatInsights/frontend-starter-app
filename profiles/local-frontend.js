16bd7b48c74a79a9d2c2b5ade40f39ac3fca643b
Ryan Long <rlong@redhat.com>
2019-06-19 10:22:02 -0400

    fixing local-frontend

--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -5,9 +5,11 @@ const APP_ID = 'starter';
 const FRONTEND_PORT = 8002;
 const routes = {};
 
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
-routes[`/${SECTION}/${APP_ID}`]      = { host: `http://localhost:${FRONTEND_PORT}` };
-routes[`/beta/apps/${APP_ID}`]       = { host: `http://localhost:${FRONTEND_PORT}` };
-routes[`/apps/${APP_ID}`]            = { host: `http://localhost:${FRONTEND_PORT}` };
+const localhost = (process.env.PLATFORM === 'linux') ? 'localhost' : 'host.docker.internal';
+
+routes[`/beta/${SECTION}/${APP_ID}`] = { host: `https://${localhost}:${FRONTEND_PORT}` };
+routes[`/${SECTION}/${APP_ID}`]      = { host: `https://${localhost}:${FRONTEND_PORT}` };
+routes[`/beta/apps/${APP_ID}`]       = { host: `https://${localhost}:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`]            = { host: `https://${localhost}:${FRONTEND_PORT}` };
 
 module.exports = { routes };
