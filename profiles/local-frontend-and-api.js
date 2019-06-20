a016129f1fdde9cf5e86d937aee2ea6bf54745c7
Ryan Long <rlong@redhat.com>
2019-06-20 09:15:15 -0400

    add https

--- profiles/local-frontend-and-api.js
+++ profiles/local-frontend-and-api.js
@@ -6,15 +6,11 @@ const FRONTEND_PORT = 8002;
 const API_PORT = 8888;
 const routes = {};
 
-// Hack so that Mac OSX docker can sub in host.docker.internal instead of localhost
-// see https://docs.docker.com/docker-for-mac/networking/#i-want-to-connect-from-a-container-to-a-service-on-the-host
-const localhost = (process.env.PLATFORM === 'linux') ? 'localhost' : 'host.docker.internal';
+routes[`/beta/${SECTION}/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/${SECTION}/${APP_ID}`]      = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/beta/apps/${APP_ID}`]       = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`]            = { host: `https://localhost:${FRONTEND_PORT}` };
 
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: `http://${localhost}:${FRONTEND_PORT}` };
-routes[`/${SECTION}/${APP_ID}`]      = { host: `http://${localhost}:${FRONTEND_PORT}` };
-routes[`/beta/apps/${APP_ID}`]       = { host: `http://${localhost}:${FRONTEND_PORT}` };
-routes[`/apps/${APP_ID}`]            = { host: `http://${localhost}:${FRONTEND_PORT}` };
-
-routes[`/api/${APP_ID}`] = { host: `http://${localhost}:${API_PORT}` };
+routes[`/api/${APP_ID}`] = { host: `https://localhost:${API_PORT}` };
 
 module.exports = { routes };
--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -5,11 +5,9 @@ const APP_ID = 'starter';
 const FRONTEND_PORT = 8002;
 const routes = {};
 
-const localhost = (process.env.PLATFORM === 'linux') ? 'localhost' : 'host.docker.internal';
-
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: `https://${localhost}:${FRONTEND_PORT}` };
-routes[`/${SECTION}/${APP_ID}`]      = { host: `https://${localhost}:${FRONTEND_PORT}` };
-routes[`/beta/apps/${APP_ID}`]       = { host: `https://${localhost}:${FRONTEND_PORT}` };
-routes[`/apps/${APP_ID}`]            = { host: `https://${localhost}:${FRONTEND_PORT}` };
+routes[`/beta/${SECTION}/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/${SECTION}/${APP_ID}`]      = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/beta/apps/${APP_ID}`]       = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`]            = { host: `https://localhost:${FRONTEND_PORT}` };
 
 module.exports = { routes };
