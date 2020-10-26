405d4d2009193440fcb1cfcb26259d658a6a1f6d
Karel Hala <khala@redhat.com>
2020-10-26 10:02:20 +0100

    Add page actions to show howcase it

--- src/Routes/NoPermissionsPage/NoPermissionsPage.js
+++ src/Routes/NoPermissionsPage/NoPermissionsPage.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 
 import { Main, NotAuthorized } from '@redhat-cloud-services/frontend-components';
 
 const NoPermissionsPage = () => {
+    useEffect(() => {
+        insights?.chrome?.appAction?.('no-permissions');
+    }, []);
+
     return (
         <Main>
             <NotAuthorized serviceName='Sample app'/>
