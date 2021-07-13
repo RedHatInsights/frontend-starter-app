badd6e5432643a74dc62e64de233b5f1eb13669d
redallen <zallen@redhat.com>
2021-07-13 12:05:35 -0400

    address richard feedback

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -2,12 +2,6 @@ const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
 const commonPlugins = require('./plugins');
 
-if (!process.env.BETA) {
-  console.warn(
-    '\u001b[31mUntil 2021-07-29 ci-stable is broken. Try `npm run start:beta` instead.\n\u001b[0m'
-  );
-}
-
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
   debug: true,
