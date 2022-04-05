5360b3914747ca3abc45a29ea592b8a04695aefd
Karel Hala <khala@redhat.com>
2022-04-05 09:45:53 +0200

    Use chrome hook to bootstrap the app

--- src/App.js
+++ src/App.js
@@ -6,24 +6,30 @@ import './App.scss';
 import { getRegistry } from '@redhat-cloud-services/frontend-components-utilities/Registry';
 import NotificationsPortal from '@redhat-cloud-services/frontend-components-notifications/NotificationPortal';
 import { notificationsReducer } from '@redhat-cloud-services/frontend-components-notifications/redux';
+import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome';
+import pckg from '../package.json';
 
 const App = (props) => {
   const history = useHistory();
+  const chrome = useChrome();
 
   useEffect(() => {
-    const registry = getRegistry();
-    registry.register({ notifications: notificationsReducer });
-    insights.chrome.init();
+    let unregister;
+    if (chrome) {
+      const registry = getRegistry();
+      registry.register({ notifications: notificationsReducer });
+      const { identifyApp, on: onChromeEvent } = chrome.init();
 
-    // TODO change this to your appname
-    insights.chrome.identifyApp('starter');
-    const unregister = insights.chrome.on('APP_NAVIGATION', (event) =>
-      history.push(`/${event.navId}`)
-    );
+      // You can use directly the name of your app
+      identifyApp(pckg.insights.appname);
+      unregister = onChromeEvent('APP_NAVIGATION', (event) =>
+        history.push(`/${event.navId}`)
+      );
+    }
     return () => {
       unregister();
     };
-  }, []);
+  }, [chrome]);
 
   return (
     <Fragment>
