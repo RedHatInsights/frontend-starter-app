87935c0faab9af08c281c58511170206ca156ae4
Martin Maroši <marvusm.mmi@gmail.com>
2023-08-02 14:54:14 +0200

    Add new build pipeline config. (#588)

--- fec.config.js
+++ fec.config.js
@@ -4,6 +4,13 @@ module.exports = {
   useProxy: true,
   proxyVerbose: true,
   /**
+   * Change accordingly to your appname in package.json.
+   * The `sassPrefix` attribute is only required if your `appname` includes the dash `-` characters.
+   * If the dash character is present, you will have add a camelCase version of it to the sassPrefix.
+   * If it does not contain the dash character, remove this configuration.
+   */
+  sassPrefix: '.frontend-starter-app, .frontendStarterApp',
+  /**
    * Change to false after your app is registered in configuration files
    */
   interceptChromeConfig: false,
---
+++ pr_check.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# --------------------------------------------
+# Export vars for helper scripts to use
+# --------------------------------------------
+# name of app-sre "application" folder this component lives in; needs to match for quay
+export COMPONENT="frontend-starter-app"
+# Needs to match the quay repo name set by app.yaml in app-interface
+export IMAGE="quay.io/cloudservices/$COMPONENT"
+export WORKSPACE=${WORKSPACE:-$APP_ROOT} # if running in jenkins, use the build's workspace
+export APP_ROOT=$(pwd)
+COMMON_BUILDER=https://raw.githubusercontent.com/RedHatInsights/insights-frontend-builder-common/master
+
+
+set -exv
+# source is preferred to | bash -s in this case to avoid a subshell
+source <(curl -sSL $COMMON_BUILDER/src/frontend-build.sh)
+BUILD_RESULTS=$?
+
+# Stubbed out for now
+mkdir -p $WORKSPACE/artifacts
+cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
+<testsuite tests="1">
+    <testcase classname="dummy" name="dummytest"/>
+</testsuite>
+EOF
+
+# teardown_docker
+exit $BUILD_RESULTS
