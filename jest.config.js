e555f5f9518144d870465ecb96e5bdedb4e5b350
Martin Marosi <marvusm.mmi@gmail.com>
2023-11-15 12:18:32 +0100

    Remove unecessary babel dependencies.

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,39 @@
+const transformIgnorePatterns = ['node_modules/(?!(uuid)/)'];
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  coverageDirectory: './coverage/',
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.js', '!src/**/stories/*'],
-  coverageDirectory: './coverage/',
+  roots: ['<rootDir>/src/'],
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
   },
-  roots: ['<rootDir>/src/'],
-  transformIgnorePatterns: [
-    '/node_modules/(?!@redhat-cloud-services)',
-    '/node_modules/(?!@patternfly)',
-  ],
-  testEnvironment: 'jest-environment-jsdom',
-  moduleDirectories: [
-    'node_modules',
-    './src', //the root directory
-  ],
+  transformIgnorePatterns,
   setupFilesAfterEnv: ['<rootDir>/config/jest.setup.js'],
+  transform: {
+    '^.+\\.(ts|js)x?$': [
+      '@swc/jest',
+      {
+        $schema: 'http://json.schemastore.org/swcrc',
+        jsc: {
+          experimental: {
+            plugins: [['jest_workaround', {}]],
+          },
+          parser: {
+            jsx: true,
+            syntax: 'typescript',
+            tsx: true,
+          },
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
+  },
 };
--- src/Components/SampleComponent/sample-component.tsx
+++ src/Components/SampleComponent/sample-component.tsx
@@ -1,10 +1,8 @@
 import './sample-component.scss';
 import React from 'react';
 
-const SampleComponent = (props: { children?: any }) => {
+const SampleComponent: React.FC<React.PropsWithChildren> = (props) => {
   return <span className="sample-component"> {props.children} </span>;
 };
-
-SampleComponent.displayName = 'SampleComponent';
 
 export default SampleComponent;
