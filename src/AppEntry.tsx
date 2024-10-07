adf5f0b16c205a3315a60959a588bb80e886c4bd
Martin Marosi <marvusm.mmi@gmail.com>
2024-10-07 13:30:20 +0200

    Dependency clean up.

--- src/AppEntry.tsx
+++ src/AppEntry.tsx
@@ -1,17 +1,6 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { init } from './store';
 import App from './App';
-import logger from 'redux-logger';
 
-const AppEntry = () => (
-  <Provider
-    store={init(
-      ...(process.env.NODE_ENV !== 'production' ? [logger] : [])
-    ).getStore()}
-  >
-    <App />
-  </Provider>
-);
+const AppEntry = () => <App />;
 
 export default AppEntry;
