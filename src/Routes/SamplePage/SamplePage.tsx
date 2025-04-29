1b1831fe0951d1bedc593a32b5851456bc16df72
Martin Marosi <marvusm.mmi@gmail.com>
2025-04-29 10:39:38 +0200

    chore(deps): Update to PF6

--- src/Routes/SamplePage/SamplePage.tsx
+++ src/Routes/SamplePage/SamplePage.tsx
@@ -22,13 +22,7 @@ import './sample-page.scss';
 import AppLink from '../../Components/AppLink';
 import { useAddNotification } from '../../state/notificationsAtom';
 
-/**
- * A smart component that handles all the api calls and data needed by the dumb components.
- * Smart components are usually classes.
- *
- * https://reactjs.org/docs/components-and-props.html
- * https://medium.com/@thejasonfile/dumb-components-and-smart-components-e7b33a698d43
- */
+
 const SamplePage = () => {
   const { appAction } = useChrome();
   const addNotification = useAddNotification();
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,13 +2,20 @@ import { To } from 'react-router-dom';
 
 export const linkBasename = '/staging/starter';
 export const mergeToBasename = (to: To, basename: string): To => {
+  console.log({ basename, to });
   if (typeof to === 'string') {
     // replace possible "//" after basename
-    return `${basename}/${to}`.replace(`^${basename}//`, '/');
+    return `${basename}/${to}`.replace(
+      new RegExp(`^${basename}//`),
+      `${basename}/`,
+    );
   }
 
   return {
     ...to,
-    pathname: `${basename}/${to.pathname}`.replace(`^${basename}//`, '/'),
+    pathname: `${basename}/${to.pathname}`.replace(
+      new RegExp(`^${basename}//`),
+      `${basename}/`,
+    ),
   };
 };
