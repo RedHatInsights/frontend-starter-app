1f985645a0b93b73764b1247db4c0dad5c746b57
Jakub007d <dr.kubko@gmail.com>
2025-11-05 15:15:32 +0100

    chore(deps): updating node version in PR check

--- pr_check.sh
+++ pr_check.sh
@@ -9,6 +9,7 @@ export COMPONENT="frontend-starter-app"
 export IMAGE="quay.io/cloudservices/$COMPONENT"
 export WORKSPACE=${WORKSPACE:-$APP_ROOT} # if running in jenkins, use the build's workspace
 export APP_ROOT=$(pwd)
+export NODE_BUILD_VERSION=20
 COMMON_BUILDER=https://raw.githubusercontent.com/RedHatInsights/insights-frontend-builder-common/master
 
 
